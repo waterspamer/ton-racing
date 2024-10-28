e8d385fbb1cbe7da5cd39fa2ad76871b1c2d4223
Maxim Sviridov <m.sviridov@kefirgames.com>
2024-10-28 18:16:19 +0300

    removed ambient light

--- scripts/garage.js
+++ scripts/garage.js
@@ -36,11 +36,11 @@ document.getElementById('garage-container').appendChild(renderer.domElement);
 
 // Добавление света
 const ambientLight = new THREE.AmbientLight(0xffffff, 1.2);
-scene.add(ambientLight);
+//scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(0, 5, 7.5);
-//scene.add(directionalLight);
+scene.add(directionalLight);
 
 // Загрузка моделей
 loadGarage(scene);
